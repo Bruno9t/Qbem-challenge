473c2e5e15480e4bc257aa88e8d2beca78e723b8
Bruno Medeiros <brudev01@gmail.com>
2023-07-25 17:18:58 -0300

    adding models for DbSet

---
+++ qbem-api/Models/Transaction.cs
@@ -0,0 +1,11 @@
+﻿namespace qbem_api.Models
+{
+    public class Transaction
+    {
+        public int Type { get; set; }
+        public string Date { get; set; }
+        public string ProductDescription { get; set; }
+        public double Value { get; set; }
+        public string SellerName { get; set; }
+    }
+}
---
+++ qbem-api/Models/TransactionType.cs
@@ -0,0 +1,10 @@
+﻿namespace qbem_api.Models
+{
+    public class TransactionType
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string Nature { get; set; }
+        public string Sinal { get; set; }
+    }
+}
